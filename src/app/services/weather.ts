941af86dd37b07b45a00f3294b3b5e7619274dc9
Sainathreddy <sai65265@gmail.com>
2025-08-05 17:37:29 +0530

    updated env

--- src/app/services/weather.ts
+++ src/app/services/weather.ts
@@ -1,13 +1,14 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class weather {
   
-  private apiKey= '118bf286ac766d1555a65763b7a6e51e';
+  private apiKey= environment.openWeatherApiKey;
   private apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
 
   constructor(private http: HttpClient) { }
