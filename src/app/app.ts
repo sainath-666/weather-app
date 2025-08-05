6a84c6e44a844bce1e6cd8a0d05b5a1d47ebc98c
Sainathreddy <sai65265@gmail.com>
2025-08-05 15:55:02 +0530

    generated weather component and services

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Weather } from "./components/weather/weather";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Weather],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
