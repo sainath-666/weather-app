8f61db00b3a22cfa005115cf4d4a27561dde16b5
Sainathreddy <sai65265@gmail.com>
2025-09-21 18:21:41 +0530

    add environment configuration files for production and development

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  openWeatherApiKey: '118bf286ac766d1555a65763b7a6e51e'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  openWeatherApiKey: '118bf286ac766d1555a65763b7a6e51e'
+};
